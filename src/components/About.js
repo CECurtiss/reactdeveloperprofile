b21842b2e654560a2bb35287f3e14e29287b24fd
CECurtiss <curtissclinton@gmail.com>
2023-02-11 22:47:40 -0600

    added components for each section of app and wrote about me section

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+function About() {
+    return (
+        <div>
+            <div className='container'>
+                <h1>About Me</h1>
+            </div>
+            <div>
+                <p>Hello! My name is Clinton Curtiss. I live in Lawrence, KS and I'm a full stack web developer. I'm an avid runner and I love being outdoors. 
+                    I enjoy challenges and I have a desire to be as proficient as possible when learning something new. I've spent a majority of my career in
+                    the liquor industry where my main duties have involved inventory management, customer service, and some social media management. Got beer
+                    questions? I'm your guy! I am absolutely excited to dig in to something new and can't wait to learn as much as I can!
+                </p>
+            </div>
+        </div>
+    )
+    }
+
+
+export default About;
