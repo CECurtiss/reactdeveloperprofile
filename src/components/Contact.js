cbff048890951241ac54bb55843038d8e20b1986
CECurtiss <curtissclinton@gmail.com>
2023-02-18 12:50:58 -0600

    added onclick events to navbar

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import Container from 'react-bootstrap/Container'
+
+function Contact() {
+return(
+    <Container>
+        <h1>Resume & Experience</h1>
+
+    </Container>
+)
+}
+
+export default Contact;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,53 @@
-import Nav from 'react-bootstrap/Nav'
+// import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import React from 'react';
 
-function Navigationbar() {
-  const tabStyle = {
+function Navigationbar(props) {
+  const { currentTab, setTab } = props;
+  
+    const tabStyle = {
     listStyle: 'none',
   }
   return (
     <>
-      <Navbar bg='dark' variant='dark'>
+      <Navbar bg='dark'>
         <Container>
           <li style={tabStyle}>
+            <a href='#About'
+            onClick={() => setTab('About')}
+            className={currentTab === 'About' ? 'nav-link active' : 'nav-link'}
+            >
             About
+            </a>
           </li>
           <li style={tabStyle}>
+          <a href='#Portfolio'
+            onClick={() => setTab('Portfolio')}
+            className={currentTab === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+            >
             Portfolio
+            </a>
           </li>
           <li style={tabStyle}>
             <h2>Clinton</h2>
             <h2>Curtiss</h2>
           </li>
           <li style={tabStyle}>
+          <a href='#Resume'
+            onClick={() => setTab('Resume')}
+            className={currentTab === 'Resume' ? 'nav-link active' : 'nav-link'}
+            >
             Resume
+            </a>
           </li>
           <li style={tabStyle}>
+          <a href='#Contact'
+            onClick={() => setTab('Contact')}
+            className={currentTab === 'Contact' ? 'nav-link active' : 'nav-link'}
+            >
             Contact
+            </a>
           </li>
         </Container>
       </Navbar>
