821d814eb43b2ee4dd028f151d1462d09cd02bec
CECurtiss <curtissclinton@gmail.com>
2023-02-21 23:11:47 -0600

    added screenshots for portfolio, some card styling" ;

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,32 +1,53 @@
 import Container from "react-bootstrap/Container";
+import Row from 'react-bootstrap/Row'
 import Card from "react-bootstrap/Card";
+import parks from '../assets/projectscreenshots/parksweather.png'
+import techBlog from '../assets/projectscreenshots/techblog.png'
+import workScheduler from '../assets/projectscreenshots/workdayscheduler.png'
+
+const cardStyle = {
+  width: '300px',
+  margin: '10px'
+}
 
 function Portfolio() {
+
+
   return (
     <Container>
-      <Card className="flip-card">
-        <Card.Img></Card.Img>
+      <Row className='flex-row'>
+      <Card style={cardStyle}>
+        <Card.Img variant='top' src={parks} />
         <Card.Body>
-          <Card.Title>Front End</Card.Title>
+          <Card.Title>National Park & Weather App</Card.Title>
           <Card.Text>project1</Card.Text>
         </Card.Body>
       </Card>
 
-      <Card>
-        <Card.Img></Card.Img>
+      <Card style={cardStyle}>
+        <Card.Img variant='top' src={techBlog} />
         <Card.Body>
-          <Card.Title>Back End</Card.Title>
+          <Card.Title>Tech Blog</Card.Title>
           <Card.Text>project2</Card.Text>
         </Card.Body>
       </Card>
 
-      <Card>
-        <Card.Img></Card.Img>
+      <Card style={cardStyle}>
+        <Card.Img variant='top' src={workScheduler} />
+        <Card.Body>
+          <Card.Title>Work Day Scheduler</Card.Title>
+          <Card.Text>project 3</Card.Text>
+        </Card.Body>
+      </Card>
+
+      <Card style={cardStyle}>
+        <Card.Img variant='top' src={workScheduler} />
         <Card.Body>
           <Card.Title>Other</Card.Title>
           <Card.Text>project 3</Card.Text>
         </Card.Body>
       </Card>
+      </Row>
     </Container>
   );
 }
